f7fb700457c4e1e76ac3c6a59a13f44e9b434197
abineshs <abinesh.s@gmail.com>
2012-06-12 18:19:23 +0530

    First commit

--- testserver.js
+++ testserver.js
@@ -4,4 +4,7 @@ server.get('/', function(req, res){
 	console.log("My first application version controlled in github, developed in cloud9 and deployed using Heroku");
 	res.send("Hello world from Cloud9, GIT and Heroku");
 });
-server.listen(8966, 'localhost');
+var port = process.env.PORT;
+server.listen( port || 5000, function(){
+    console.log("Listening on Port"+port);
+});
