5fc53d678d7c50cf348f9ca44cac1cc1ec78ab82
abineshs <abinesh.s@gmail.com>
2012-06-12 17:50:21 +0530

    For heroku deploy

--- testserver.js
+++ testserver.js
@@ -4,4 +4,4 @@ server.get('/', function(req, res){
 	console.log("My first application version controlled in github, developed in cloud9 and deployed using Heroku");
 	res.send("Hello world from Cloud9, GIT and Heroku");
 });
-server.listen(8966, '10.1.10.55');
+server.listen(8966, 'localhost');
